a0ac6df505c59876306a87a3c70be4e533f77a62
Thomas-2003 <thomasroenneper12@web.de>
2022-03-09 09:28:06 +0100

    extent server with todos json responding endpoint

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
-    res.send('Hello 222222!')
+    console.log(`GET / at ${Date.now()}`)
+
+    res.send('Hello World!')
+})
+app.get('/todos', (req, res) => {
+    const jsonResponse = [{
+        title: "bread",
+        id: 1,
+        completed: false
+    },
+    {
+        title: "milk",
+        id: 2,
+        completed: false
+    },
+    {
+        title: "beer",
+        id: 3,
+        completed: false
+    }]
+    console.log(`GET /todos at ${Date.now()}`)
+    res.json(jsonResponse)
 })
 
 app.listen(port, () => {
